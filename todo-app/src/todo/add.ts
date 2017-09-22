bd0e4d1b18803667d6369d802193f222bbdc09e5
Torben Knerr <mail@tknerr.de>
2017-09-22 12:03:31 +0200

    add binding to the todo properties

--- todo-app/src/todo/add.ts
+++ todo-app/src/todo/add.ts
@@ -1,13 +1,21 @@
 import { TodoService } from './todo-service';
+import { Todo } from './todo';
 import { autoinject } from 'aurelia-framework';
 
 @autoinject()
 export class Add {
+
+  // create an empty todo item which we can bind the form fields to
+  // note that the binding is bi-directional, i.e. the values we initialize
+  // here will also be shown in the UI
+  todo = new Todo('', '', 'Medium')
+
   constructor(
     private todoService: TodoService
   ) { }
 
   addTodo() {
-    console.log("heck yes, let's add it!")
+    console.log("heck yes, let's add it to the list!")
+    console.log(this.todo)
   }
 }
