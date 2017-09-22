6af2336044c63adb6066e7050dca0e976b26cf9c
Torben Knerr <mail@tknerr.de>
2017-09-22 11:01:09 +0200

    replace message with router

--- todo-app/src/app.ts
+++ todo-app/src/app.ts
@@ -1,4 +1,14 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+
 export class App {
-  // a simple string property, used in app.html
-  message = 'Hello Aurelia!';
+  router = null;
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'todo'], name: 'Todos', moduleId: 'todo/index' },
+      { route: 'todo/add', name: 'Add new todo', moduleId: 'todo/add' }
+    ]);
+  }
 }
